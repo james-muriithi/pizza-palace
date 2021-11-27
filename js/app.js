877cff513c782b0bc8932c493793203a5ae32cb6
james-muriithi <muriithijames556@gmail.com>
2021-11-28 02:03:31 +0300

    add bg to navbar on scroll

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const nav = $("nav");
+
+$(function () {
+    $('body').on("scroll", changeNavbarClass);
+
+});
+
+
+changeNavbarClass()
+
+function changeNavbarClass() {
+  const whyChooseUsSection = $("#why-choose-us").offset().top;
+
+  if ($(window).scrollTop() > whyChooseUsSection) {
+    nav.addClass("inverse");
+  } else {
+    nav.removeClass("inverse");
+  }
+}
